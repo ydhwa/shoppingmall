0ecbe7497032bc10a4f77d55523a3f17dddad338
ydhwa <ydhwa_18@naver.com>
2019-07-11 03:02:11 +0900

    MemberServiceTest에 따라 service 수정

--- src/main/java/com/cafe24/shoppingmall/service/MemberService.java
+++ src/main/java/com/cafe24/shoppingmall/service/MemberService.java
@@ -1,5 +1,7 @@
 package com.cafe24.shoppingmall.service;
 
+import java.util.regex.Pattern;
+
 import org.springframework.stereotype.Service;
 
 import com.cafe24.shoppingmall.exception.ExceptionMessage;
@@ -25,18 +27,39 @@ public class MemberService {
 		memberVo.setStatus(MemberStatus.ENABLE);
 		memberVo.setRole(MemberRole.USER);
 		
+		// Dao 필요
+		
 		return memberVo;
 	}
-	private boolean checkMissingData(MemberVo memberVo) {
-		
-		return false;
-	}
 	private boolean checkValidation(MemberVo memberVo) {
+		// 아이디) 4~12자 영문, 숫자, _(언더바) 자유 조합
+		String usernamePattern = "^[A-Za-z0-9_]{4,12}$";
+		// 비밀번호) 8~20자 영문, 숫자, 특수문자 1회 이상 포함
+		String passwordPattern = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,20}$";
 		
-		return false;
+		// 아이디 검사
+		if(!memberVo.getUsername().matches(usernamePattern)) { return false; }
+		
+		// 비밀번호 검사
+		if(!memberVo.getPassword().matches(passwordPattern)) { return false; }
+		
+		return true;
+	}
+	private boolean checkMissingData(MemberVo memberVo) {
+		if(memberVo.getUsername() == null) { return false; }
+		if(memberVo.getPassword() == null) { return false; }
+		if(memberVo.getName() == null) { return false; }
+		if(memberVo.getBirthDate() == null) { return false; }
+		if(memberVo.getPhoneNumber() == null) { return false; }
+		if(memberVo.getEmail() == null) { return false; }
+		if(memberVo.getSmsReception() == null) { return false; }
+		if(memberVo.getEmailReception() == null) { return false; }
+				
+		return true;
 	}
 	
 	public Object checkUsername(String username) {
+		// Dao 필요
 		
 		return username;
 	}
