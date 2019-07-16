2ade664fa8514fa2bca996c768b7e30d0ea70597
ydhwa <ydhwa_18@naver.com>
2019-07-16 20:04:45 +0900

    serial8 -> integer로 타입 변경하고 sequence 추가 (문제 없는 것들은 serial8로 선언함)

---
+++ docs/define_sequence_w3-1.sql
@@ -0,0 +1,23 @@
+-- member
+drop sequence seq_member_no;
+create sequence seq_member_no;
+
+-- product
+drop sequence seq_product_no;
+create sequence seq_product_no;
+
+-- orders
+drop sequence seq_orders_no;
+create sequence seq_orders_no;
+
+-- bucket
+drop sequence seq_bucket_no;
+create sequence seq_bucket_no;
+
+-- product_option_item
+drop sequence seq_product_option_item_no;
+create sequence seq_product_option_item_no;
+
+-- category
+drop sequence seq_category_no;
+create sequence seq_category_no;
